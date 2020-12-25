f5fe81833216caf029149553ef4ea230680d6471
chad <canddx3@gmail.com>
2020-12-24 21:56:28 -0600

    stopped at table ngFOR

---
+++ src/app/orbit-list/orbit-list.component.ts
@@ -0,0 +1,16 @@
+import { Satellite } from '../satellite';
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-orbit-list',
+  templateUrl: './orbit-list.component.html',
+  styleUrls: ['./orbit-list.component.css']
+})
+export class OrbitListComponent implements OnInit {
+  @Input() satellites: Satellite[];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
